dee328863a77579d50ccc671841f1c8170c53ed9
BouhlelRochdi <rochdi.bouhlel@hotmail.fr>
2022-12-18 02:51:59 +0100

    conection to database & create libs {first edit}

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as passport from 'passport';
+
+import { environment } from './environment/environment';
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  //await app.listen(3000);
+
+  /*app.setGlobalPrefix('api');
+  //passport sessions
+  app.use(passport.initialize());
+  app.use(passport.session());
+*/
+  const apiPort = environment.apiPort;
+  await app.listen(apiPort, () => {
+    console.log('Listening at http://localhost:', apiPort);
+  });
+
 }
 bootstrap();
